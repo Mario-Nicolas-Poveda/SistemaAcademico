fe67307011b023f9fbe7ce8c6c6efc3aecbe72f7
Mario Poveda <mnpoveda@unicesar.edu.co>
2026-02-27 17:04:48 -0500

    feat: crear estructura base de Main con ArrayLists

---
+++ src/main/java/com/mycompany/sistemaacademico/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemaacademico;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author Mario
+ */
+public class Main {
+    private static ArrayList<Estudiante> estudiantes = new ArrayList<>();
+    private static ArrayList<Asignatura> asignaturas = new ArrayList<>();
+    private static ArrayList<Nota> notas = new ArrayList<>();
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido");
+    }
+}
