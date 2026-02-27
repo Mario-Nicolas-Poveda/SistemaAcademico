fce7ddd90ee2d66a74a6fda9a2c3409fed1568cb
Mario Poveda <mnpoveda@unicesar.edu.co>
2026-02-27 16:23:40 -0500

    feat: inicializar proyecto SistemaAcademico

---
+++ src/main/java/com/mycompany/sistemaacademico/SistemaAcademico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemaacademico;
+
+/**
+ *
+ * @author Mario
+ */
+public class SistemaAcademico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
