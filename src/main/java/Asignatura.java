0fc96e71efba0a0e5ad4c14b3d7e6abe525874b6
Jose Gomez <jjuliogomez@unicesar.edu.co>
2026-03-02 08:43:26 +0900

    feat: crear clase Asignatura con atributos v2

--- src/main/java/Asignatura.java
+++ src/main/java/Asignatura.java
@@ -1,12 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Jose
- */
-public class Asignatura {
+public class Asignatura 
+{
+    private String codigo;
+    private String nombre;
+    private int creditos;
+    private String docente;
     
 }
