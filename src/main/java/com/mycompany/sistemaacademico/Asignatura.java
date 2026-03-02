dec36aad573df40f440f0e5d3e622e26b8517cf3
rcamilorodriguez1 <rcamilorodriguez@unicesar.edu.co>
2026-03-01 19:34:58 -0500

    feat: implementar CREATE - registrar asignatura

---
+++ src/main/java/com/mycompany/sistemaacademico/Asignatura.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemaacademico;
+
+/**
+ *
+ * @author Jose Rodriguez
+ */
+public class Asignatura {
+    private String codigo;
+    private String nombre;
+    private int creditos;
+
+    public Asignatura(String codigo, String nombre, int creditos) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    @Override
+    public String toString() {
+        return "Asignatura{" +
+                "codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", creditos=" + creditos +
+                '}';
+    }
+}
--- src/main/java/com/mycompany/sistemaacademico/SistemaAcademico.java
+++ src/main/java/com/mycompany/sistemaacademico/SistemaAcademico.java
@@ -4,10 +4,37 @@
  */
 package com.mycompany.sistemaacademico;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
 /**
  *
  * @author Jose Rodriguez
  */
 public class SistemaAcademico {
-    
+    static ArrayList<Asignatura> listaAsignaturas = new ArrayList<>();
+        static Scanner sc = new Scanner(System.in);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         registrarAsignatura();
+        
+}
+    public static void registrarAsignatura() {
+        System.out.print("Código: ");
+        String codigo = sc.nextLine();
+
+        System.out.print("Nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Créditos: ");
+        int creditos = sc.nextInt();
+        sc.nextLine();
+
+        listaAsignaturas.add(new Asignatura(codigo, nombre, creditos));
+        System.out.println("Asignatura registrada correctamente.");
+    }
 }
